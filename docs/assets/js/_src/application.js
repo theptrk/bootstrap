c18684024acd2afe80dafa577c8ebfc2c51ef011
XhmikosR <xhmikosr@users.sourceforge.net>
2014-03-20 07:26:37 +0200

    Update ZeroClipboard to v1.3.5.
    Also use the official filenames.

--- docs/assets/js/_src/application.js
+++ docs/assets/js/_src/application.js
@@ -86,7 +86,7 @@
 
     // Config ZeroClipboard
     ZeroClipboard.config({
-      moviePath: '/assets/flash/zero-clipboard.swf',
+      moviePath: '/assets/flash/ZeroClipboard.swf',
       hoverClass: 'btn-clipboard-hover'
     })
 
